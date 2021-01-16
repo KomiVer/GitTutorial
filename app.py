1b31dc0d431eef68be29ea6414286a8a5a3ea8b1
tfe <Takfes@users.noreply.github.com>
2021-01-16 18:35:57 +0200

    revert to older version

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import pandas as pd
 import numpy as np
 
-
-if __name__ == '__main__':
-    print('Hello from Python')
-    print(pd.__version__)
-    print(np.__version__)
+print('Hello world')
+print(pd.__version__)
